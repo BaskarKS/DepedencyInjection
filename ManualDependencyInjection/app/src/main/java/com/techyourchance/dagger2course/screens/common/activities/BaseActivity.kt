8d8d2c2d3e39fdd599289cfe189c214b17e8ff62
Baskar K S <baskarks@gmail.com>
2020-12-29 16:08:28 +0530

    Adding Manual Dependency Injection Code

---
+++ ManualDependencyInjection/app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.techyourchance.dagger2course.screens.common.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import com.techyourchance.dagger2course.MyApplication
+import com.techyourchance.dagger2course.common.dependnecyinjection.ActivityCompositionRoot
+import com.techyourchance.dagger2course.common.dependnecyinjection.Injector
+import com.techyourchance.dagger2course.common.dependnecyinjection.PresentationCompositionRoot
+
+open class BaseActivity: AppCompatActivity() {
+
+    private val appCompositionRoot get() = (application as MyApplication).appCompositionRoot
+
+    val activityCompositionRoot by lazy {
+        ActivityCompositionRoot(this, appCompositionRoot)
+    }
+
+    private val compositionRoot by lazy {
+        PresentationCompositionRoot(activityCompositionRoot)
+    }
+
+    protected val injector get() = Injector(compositionRoot)
+}
